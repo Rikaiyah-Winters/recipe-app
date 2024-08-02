ae977827491dbb14592bf6adf233834598e01566
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2024-08-01 19:51:38 -0500

    Built recipe exerpts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
+import RecipeExcerpt from "./components/RecipeExcerpt"
 import "./App.css";
 
 function App() {
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    const fetchAllRecipes = async () => {
+      try {
+        const response = await fetch("/api/recipes");
+        if (response.ok) {
+          const recipesData = await response.json();
+          setRecipes(recipesData)
+        } else {
+          console.log("Couldn't fetch recipes.")
+        }
+      } catch (e) {
+        console.error("An error occured during the request", e)
+        console.log("An unexpected error occured. Please try again later.")
+      }
+    };
+
+    fetchAllRecipes();
+  }, [])
+
   return (
     <div className='recipe-app'>
       <Header />
-      <p>Your recipes here! </p>
+      <div className="recipe-list">
+        {recipes.map((recipe) => (
+          <RecipeExcerpt key={recipe.id} recipe={recipe}/>
+        ))}
+      </div>
     </div>
-  );
+      );
 }
 
-export default App;
+      export default App;
