a9014e501882ac4ad00f037884900e71b0bb11dc
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2024-08-02 09:49:43 -0500

    Finished Creating a new recipe functionality

--- src/App.js
+++ src/App.js
@@ -59,14 +59,52 @@ function App() {
     fetchAllRecipes();
   }, [])
 
+  //wouldn't use useEffect here since this funciton will only trigger when it's called, not every time the page loads
+  const handleNewRecipe = async (e, newRecipe) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/recipes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newRecipe)
+      });
+      if (response.ok) {
+        const recipesData = await response.json();
+        setRecipes([...recipes, recipesData.recipe]);
+        console.log("Recipe added sucessfully!");
+        setShowNewRecipeForm(false);
+        setNewRecipe({
+          title: "",
+          ingredients: "",
+          instructions: "",
+          servings: 1,
+          description: "",
+          image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        });
+      } else {
+        console.error("Couldn't fetch recipes.")
+      }
+    } catch (e) {
+      console.error("An error occured during the request", e)
+    }
+  };
+
+
   return (
     <div className='recipe-app'>
       <Header showRecipeForm={showRecipeForm} />
-      {showNewRecipeForm && <NewRecipeForm newRecipe={newRecipe} hideRecipeForm={hideRecipeForm} onUpdateForm={onUpdateForm} />}
+      {showNewRecipeForm && 
+      <NewRecipeForm newRecipe={newRecipe} 
+      hideRecipeForm={hideRecipeForm} 
+      onUpdateForm={onUpdateForm} 
+      handleNewRecipe={handleNewRecipe} />}
+
       {selectedRecipe && <RecipeFull
         selectedRecipe={selectedRecipe}
         handleUnselectRecipe={handleUnselectRecipe} />}
-      {!selectedRecipe && (<div className="recipe-list">
+      {!selectedRecipe && !showNewRecipeForm && (<div className="recipe-list">
         {recipes.map((recipe) => (
           <RecipeExcerpt
             key={recipe.id}
